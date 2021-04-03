64cad54eb71d039687fe4c62dc9818f82f7a1707
Jom185 <76267367+Jom185@users.noreply.github.com>
2021-04-03 17:38:30 +0100

    Improved styling for nested steps

--- src/components/ItemSteps/ItemStep/ItemStep.js
+++ src/components/ItemSteps/ItemStep/ItemStep.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import TextButton from "../../UI/TextButton/TextButton";
 import ItemSteps from "../ItemSteps";
 import "./ItemStep.css";
 
@@ -11,9 +12,17 @@ const ItemStep = (props) => {
       <div className="main">
         <li>{step}</li>
         {subSteps && (
-          <div onClick={() => setIsExpanded(!isExpanded)}>
-            <strong>{isExpanded ? "-" : "+"}</strong>
-          </div>
+          <TextButton
+            tip={isExpanded ? "Show Less" : "Show More"}
+            onClick={() => setIsExpanded(!isExpanded)}
+          >
+            {" "}
+            <div
+              className={`expand-arrow ${
+                isExpanded ? "toggle-down" : "toggle-up"
+              }`}
+            ></div>
+          </TextButton>
         )}
       </div>
       {subSteps && isExpanded && (
